5440083f32bcf67a74a4b5652482399f887251c0
youssefbadr <youssef.abdaalla.16@ucl.ac.uk>
2024-12-31 12:59:56 +0000

    Update package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="vect_gan",
-    version="0.1.0",
+    version="0.1.1",
     description="A package for generating synthetic molecular tabular data using VECT-GAN.",
     long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
